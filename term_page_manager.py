b8c3fbe105b83857048099df1ce4efec59749632
Dan Lu <dan.lu@sagebase.org>
2023-11-07 15:06:56 -0800

    add function descriptions

--- term_page_manager.py
+++ term_page_manager.py
@@ -17,7 +17,7 @@ import pandas as pd
 from mdutils import fileutils
 
 
-def get_term_info(data_model,term):
+def get_term_info(data_model, term):
     """
     Function to get a dictionary for term definition, definition source, module
     :param data_model (pd.DataFrame): data model data frame
@@ -31,6 +31,7 @@ def get_term_info(data_model,term):
     ].to_dict("records")
     return results
 
+
 def generate_page(data_model, term_path):
     """
     Function to generate term/template markdown page
@@ -41,83 +42,92 @@ def generate_page(data_model, term_path):
     :returns: a term Markdown page generated under the docs/<module_name> folder
     """
     # get term information
-    term = term_path.split('/')[-1].split('.')[0]
-    module = term_path.split('/')[1]
-    if 'json' in term_path: 
+    term = term_path.split("/")[-1].split(".")[0]
+    module = term_path.split("/")[1]
+    if "json" in term_path:
         # term with JSON files
         with open(term_path) as f:
             results = json.load(f)
-            description = results['description']
-            source = results['$id']
+            description = results["description"]
+            source = results["$id"]
     else:
         # term with csv files
         results = get_term_info(data_model, term)
-        description = results[0]['Description']
-        source = results[0]['Source']
-    
+        description = results[0]["Description"]
+        source = results[0]["Source"]
+
     # add source url for Sage Bionetworks
     if source == "Sage Bionetworks":
         source = "https://sagebionetworks.org/"
 
-    if 'Template' in term:
+    if "Template" in term:
         # load template
         post = frontmatter.load("template_page_template.md")
         # convert camel case to title
-        post.metadata["title"] = re.sub('([A-Z]+)', r' \1', term).title()
-        post.metadata['permalink'] = f'docs/{post.metadata["title"]}.html'
-    elif 'json' in term_path:    
+        post.metadata["title"] = re.sub("([A-Z]+)", r" \1", term).title()
+        post.metadata["permalink"] = f'docs/{post.metadata["title"]}.html'
+    elif "json" in term_path:
         # load template
         post = frontmatter.load("json_term_page_template.md")
         post.metadata["title"] = term
     else:
         post = frontmatter.load("csv_term_page_template.md")
         post.metadata["title"] = term
-    post.metadata["parent"] = re.sub('([A-Z]+)', r' \1', module).title()
+    post.metadata["parent"] = re.sub("([A-Z]+)", r" \1", module).title()
 
     # load input data and term/template description
-    if 'Template' in term:
+    if "Template" in term:
         post.content = (
-        "{% assign mydata=site.data."
-        + f"{module}."
-        + f"{term}"
-        + " %} \n{: .note-title } \n"
-        + f">{post.metadata['title']}\n"
-        + ">\n"
-        + f">{description} [[Source]]({source})\n"
-        + post.content
-    )
-    else: 
+            "{% assign mydata=site.data."
+            + f"{module}."
+            + f"{term}"
+            + " %} \n{: .note-title } \n"
+            + f">{post.metadata['title']}\n"
+            + ">\n"
+            + f">{description} [[Source]]({source})\n"
+            + post.content
+        )
+    else:
         post.content = (
-        "{% assign mydata=site.data."
-        + f"{module}."
-        + f"{term}.anyOf"
-        + " %} \n"
-        + "{% assign module=site.data."
-        + f"{module}."
-        + f"{term}"
-        + " %} "
-        + "\n{: .note-title } \n"
-        + f">{post.metadata['title']}\n"
-        + ">\n"
-        + f">{description} [[Source]]({source})\n"
-        + post.content
-    )
+            "{% assign mydata=site.data."
+            + f"{module}."
+            + f"{term}.anyOf"
+            + " %} \n"
+            + "{% assign module=site.data."
+            + f"{module}."
+            + f"{term}"
+            + " %} "
+            + "\n{: .note-title } \n"
+            + f">{post.metadata['title']}\n"
+            + ">\n"
+            + f">{description} [[Source]]({source})\n"
+            + post.content
+        )
 
     # create directory for the moduel if not exist
     if not os.path.exists(f"docs/{post.metadata['parent']}/"):
         os.mkdir(f"docs/{post.metadata['parent']}/")
         # create a module page
-        module_page = fileutils.MarkDownFile(f"docs/{post.metadata['parent']}/{post.metadata['parent']}")
-        if 'Template' in term:
+        module_page = fileutils.MarkDownFile(
+            f"docs/{post.metadata['parent']}/{post.metadata['parent']}"
+        )
+        if "Template" in term:
             # add permalink for template page
-            module_page.append_end(f"--- \nlayout: page \ntitle: {post.metadata['parent']} \nhas_children: true \nnav_order: 5 \npermalink: docs/{post.metadata['parent']}.html \n---")
-        else: 
-            module_page.append_end(f"--- \nlayout: page \ntitle: {post.metadata['parent']} \nhas_children: true \nnav_order: 2 \npermalink: docs/{post.metadata['parent']}.html \n---")
-    
+            module_page.append_end(
+                f"--- \nlayout: page \ntitle: {post.metadata['parent']} \nhas_children: true \nnav_order: 5 \npermalink: docs/{post.metadata['parent']}.html \n---"
+            )
+        else:
+            module_page.append_end(
+                f"--- \nlayout: page \ntitle: {post.metadata['parent']} \nhas_children: true \nnav_order: 2 \npermalink: docs/{post.metadata['parent']}.html \n---"
+            )
+
     # create file
-    file = fileutils.MarkDownFile(f"docs/{post.metadata['parent']}/{post.metadata['title']}")
+    file = fileutils.MarkDownFile(
+        f"docs/{post.metadata['parent']}/{post.metadata['title']}"
+    )
     # add content to the file
     file.append_end(frontmatter.dumps(post))
+
 
 def delete_page(term):
     """
@@ -128,25 +138,35 @@ def delete_page(term):
     :returns: delete the term Markdown page in the docs/<module_name> folder
     """
     for file in glob.glob("docs/**/*.md"):
-        if file.split('/')[-1].split('.')[0] == term:
+        if file.split("/")[-1].split(".")[0] == term:
             os.remove(file)
+
 
 def main():
     # load data model csv file
     data_model = pd.read_csv("veoibd.data.model.csv")
-    # pull terms 
-    term_files = [file.split('/')[-1].split('.')[0] for file in glob.glob('_data/**/*')]
-    term_pages = [file.split('/')[-1].split('.')[0] for file in glob.glob("docs/**/*.md")]
-    to_add = np.setdiff1d(term_files,term_pages).tolist()
-    to_delete = np.setdiff1d(term_pages,term_files).tolist()
-    to_add_path = [file for term in to_add for file in glob.glob("_data/**/*") if file.split('/')[-1].split('.')[0] == term]
+    # pull terms
+    term_files = [file.split("/")[-1].split(".")[0] for file in glob.glob("_data/**/*")]
+    term_pages = [
+        file.split("/")[-1].split(".")[0] for file in glob.glob("docs/**/*.md")
+    ]
+    to_add = np.setdiff1d(term_files, term_pages).tolist()
+    to_delete = np.setdiff1d(term_pages, term_files).tolist()
+    to_add_path = [
+        file
+        for term in to_add
+        for file in glob.glob("_data/**/*")
+        if file.split("/")[-1].split(".")[0] == term
+    ]
     # generate pages for terms with the term files
     generate_page_temp = partial(generate_page, data_model)
     list(map(generate_page_temp, to_add_path))
     # delete pages for terms without the term files and exclude module page
-    to_delete = [x for x in to_delete if x not in data_model['Module'].dropna().unique().tolist()]
+    to_delete = [
+        x for x in to_delete if x not in data_model["Module"].dropna().unique().tolist()
+    ]
     list(map(delete_page, to_delete))
+
 
 if __name__ == "__main__":
     main()
-
